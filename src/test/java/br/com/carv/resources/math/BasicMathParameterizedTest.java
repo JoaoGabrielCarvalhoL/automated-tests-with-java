6429a11f7debcf9b7b9e1f2312b16387bac25195
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2023-09-26 10:51:53 -0300

    Example with ParameterizedTest & MethodSource

---
+++ src/test/java/br/com/carv/resources/math/BasicMathParameterizedTest.java
@@ -0,0 +1,29 @@
+package br.com.carv.resources.math;
+
+import java.math.BigDecimal;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class BasicMathParameterizedTest {
+
+	@ParameterizedTest
+	@MethodSource("test_div_input")
+	void test_div(BigDecimal dividend, BigDecimal divider, BigDecimal expected) {
+		BasicMath basicMath = new BasicMath();
+		BigDecimal result = basicMath.div(dividend, divider);
+		Assertions.assertEquals(expected, result);
+	}
+	
+	public static Stream<Arguments> test_div_input() {
+		return Stream.of(
+				Arguments.of(new BigDecimal(10), new BigDecimal(5), new BigDecimal(2)),
+				Arguments.of(new BigDecimal(15), new BigDecimal(5), new BigDecimal(3)), 
+				Arguments.of(new BigDecimal(5), new BigDecimal(5), new BigDecimal(1)));
+				
+	}
+
+}
