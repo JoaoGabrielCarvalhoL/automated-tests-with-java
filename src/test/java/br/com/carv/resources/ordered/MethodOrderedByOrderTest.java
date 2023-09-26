638f834f0d277b37264636446904c114af968ece
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2023-09-26 12:18:16 -0300

    Example Test Method Ordered By Order

--- src/test/java/br/com/carv/resources/ordered/MethodOrderedByOrderTest.java
+++ src/test/java/br/com/carv/resources/ordered/MethodOrderedByOrderTest.java
@@ -3,41 +3,35 @@ package br.com.carv.resources.ordered;
 import java.util.logging.Logger;
 
 import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class MethodOrderedByNameTest {
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class MethodOrderedByOrderTest {
 
-	Logger logger = Logger.getLogger(MethodOrderedByNameTest.class.getName());
+	Logger logger = Logger.getLogger(MethodOrderedByOrderTest.class.getName());
 	
 	@Test
-	@Order(1)
 	void testA() {
 		logger.info("First");
 	}
 	
 	@Test
-	@Order(4)
 	void testB() {
 		logger.info("Second");
 	}
 	
 	@Test
-	@Order(2)
 	void testC() {
 		logger.info("Fourth");
 	}
 	
 	@Test
-	@Order(5)
 	void testD() {
 		logger.info("Fifth");
 	}
 	
 	@Test
-	@Order(3)
 	void testE() {
 		logger.info("Sixth");
 	}
