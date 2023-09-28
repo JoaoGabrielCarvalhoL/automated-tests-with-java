9c012df655bcf1c959e6768322ae194f737d42f1
Joao Gabriel Carvalho <27.joaogabriel@gmail.com>
2023-09-27 23:22:33 -0300

    Add suport to Spring Application

---
+++ src/main/java/br/com/carv/AutomatedTestsApplication.java
@@ -0,0 +1,11 @@
+package br.com.carv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTestsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedTestsApplication.class);
+	}
+}
